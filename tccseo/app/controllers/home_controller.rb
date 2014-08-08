c89e867965c6cbd16718ba97d340d7b55c6518e3
Jean Miguel <jeanmiguelfdias@gmail.com>
2014-08-07 23:51:47 -0300

    Fixed bug on sitemap

--- tccseo/app/controllers/home_controller.rb
+++ tccseo/app/controllers/home_controller.rb
@@ -18,33 +18,33 @@ class HomeController < ApplicationController
               ["0.5", "http://www.tccseo.com.br/contato", "daily"],
             ]
 
-    categorias = Categoria.where(status: 1)
-    artigos = Artigo.where(status: 1)
-    calendarios = Calendario.all
+        categorias = Categoria.where(status: 1)
+        artigos = Artigo.where(status: 1)
+        calendarios = Calendario.all
 
-    @output = Array.new
+        @output = Array.new
 
-    urls.each do |u|
-    	@output << [u[1], u[2], u[0]]
-    end
+        urls.each do |u|
+        	@output << [u[1], u[2], u[0]]
+        end
 
-    url_base = 'http://www.tccseo.com.br/'
+        url_base = 'http://www.tccseo.com.br/'
 
-    categorias.each do |c|
-    	@output << [url_base.to_s + c.url , 'daily', '0.8']
-    end
+        categorias.each do |c|
+        	@output << [url_base.to_s + c.url_amigavel , 'daily', '0.8']
+        end
 
-    artigos.each do |a|
-    	@output << [url_base.to_s + a.categoria.url + '/' + a.url_amigavel , 'daily', '0.65']
-    end
+        artigos.each do |a|
+        	@output << [url_base.to_s + a.categoria.url_amigavel + '/' + a.url_amigavel , 'daily', '0.65']
+        end
 
-    calendarios.each do |c|
-    	@output << [url_base.to_s + 'evento' + c.url_amigavel, 'daily', '0.5' ]
-    end
+        calendarios.each do |c|
+        	@output << [url_base.to_s + 'evento' + c.url_amigavel, 'daily', '0.5']
+        end
 
-    headers['Content-Type'] = 'application/xml'
+        headers['Content-Type'] = 'application/xml'
 
-    render 'sitemap', :layout => false
+        render 'sitemap', :layout => false
 	end
 
 end
--- tccseo/app/helpers/application_helper.rb
+++ tccseo/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def root_url
 		if ENV['RAILS_ENV']=='production'
-			'http://tccseo.herokuapp.com'
+			'http://www.tccseo.com.br'
 		elsif ENV['RAILS_ENV']=='development'
 			'http://127.0.0.1:3000'
 		end
